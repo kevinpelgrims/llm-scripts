a7d06d84594a554294a91f75a80cdf7efbadcb5e
Kevin Pelgrims <kevin.pelgrims@gmail.com>
2024-02-15 10:24:35 +0100

    Update commit message generator
    Add support for Git's arguments and a model argument that can be added
    when calling the script manually.
    Write changes to Git's commit message file, so that the generated title
    and message show up in the editor by default when committing.

--- src/gitCommitMessage.js
+++ src/gitCommitMessage.js
@@ -1,16 +1,25 @@
 #!/usr/bin/env node
 
+/**
+ * When preparing a commit message, run the diff through an LLM to
+ * generate the commit title and message automatically.
+ */
+
 import { execSync } from 'child_process'
 
 import dotenv from 'dotenv'
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
+import fs from 'fs'
 
 dotenv.config()
 
 const ollamaBaseUrl = process.env.OLLAMA_BASE_URL
+const defaultModel = 'llama2'
 
-function generateRequest(diff) {
+function generateRequest(diff, model) {
   const url = getUrl()
-  const body = generateBody(diff)
+  const body = generateBody(diff, model)
 
   return fetch(url, {
     method: 'POST',
@@ -26,7 +35,7 @@ function getUrl() {
   return `${ollamaBaseUrl}/api/generate`
 }
 
-function generateBody(diff) {
+function generateBody(diff, model) {
   const instructions =
     'You take in Git diffs and summarize them into a commit title and message. ' +
     'The title should be maximum 50 characters long, and the message van span multiple lines with a maximum length of 75 characters per line. ' +
@@ -34,14 +43,60 @@ function generateBody(diff) {
   const prompt = `${instructions} This is the diff: ${diff}`
 
   return {
-    model: 'llava',
+    model: model,
     stream: false,
     format: 'json',
     prompt: prompt,
   }
 }
 
+function writeCommitMessageToFile(file, title, message) {
+  const existingContent = fs.readFileSync(file)
+  const finalContent = `${title}\n\n${message}\n\n${existingContent}`
+  fs.writeFileSync(file, finalContent)
+}
+
 export async function main(argv) {
+  const options = await yargs(hideBin(argv))
+    .usage('$0 [file] [source] [hash] [args]')
+    .command(
+      '$0 [file] [source] [hash]',
+      'Generate commit message',
+      (yargs) => {
+        yargs
+          .positional('file', {
+            type: 'string',
+            describe: 'The file that contains the commit log message',
+          })
+          .positional('source', {
+            type: 'string',
+            describe:
+              'The source of the commit message\nPossibilities are message, template, merge, squash or commit',
+          })
+          .positional('hash', {
+            type: 'string',
+            describe: 'The id of the commit',
+          })
+      },
+    )
+    .option('model', {
+      type: 'string',
+      alias: 'm',
+      describe: 'The model to use for summarizing the diff',
+      default: defaultModel,
+    })
+    .version('1.0.0')
+    .help()
+    .alias('help', 'h')
+    .parse()
+
+  if (options.source && options.source.length > 0) {
+    console.log('Not generating commit message due to source of commit')
+    // We shouldn't generate a commit message here because of the source of the
+    // commit (e.g. we don't want to override the existing message for a squash)
+    return 0
+  }
+
   const stagedFiles = execSync('git diff --staged --name-only')
   const stagedDiff = execSync('git diff --staged')
 
@@ -54,11 +109,18 @@ export async function main(argv) {
     `Generating commit message for staged files:\n${stagedFiles.toString()}`,
   )
 
-  const request = generateRequest(stagedDiff)
+  const request = generateRequest(stagedDiff, options.model)
   const response = await request
   const result = await response.json()
 
-  console.log(result.response)
+  if (!result.response) {
+    console.log('No valid response received from the model', '\n', response)
+    return 0
+  }
+
+  const commit = JSON.parse(result.response)
+
+  writeCommitMessageToFile(options.file, commit.title, commit.message)
 
   return 0
 }
